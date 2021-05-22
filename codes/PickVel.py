9fc19c170fe689ed35454e8afd66af8866f10e1e
Wang Zehao <61357287+Addasecond86@users.noreply.github.com>
2021-05-22 12:01:57 +0800

    Code upload

--- codes/PickVel.py
+++ codes/PickVel.py
@@ -1,6 +1,8 @@
 import numpy as np
 from matplotlib import pyplot as plt
 from numpy import array
+from scipy.interpolate import interp1d, UnivariateSpline
+from scipy.optimize import curve_fit
 from sklearn import preprocessing
 from sklearn.cluster import DBSCAN
 from sklearn.cluster import KMeans
@@ -318,11 +320,69 @@ class ImpeccableCluster:
 		cluster.fit(self.data)
 		if cluster.converged_:
 			print('恭喜! 算法收敛!')
-			self.centers = cluster.means_
+			self.rm_repeat_point(cluster.means_)
 			self.labels = False
-			self.cluster_number = len(np.unique(self.centers[:, 1:2]))
 		else:
 			print('不好意思哈, 算法没收敛, 请重试或更改参数后重试!')
+
+	def rm_repeat_point(self, target):
+		"""
+		去除重复的点
+		"""
+		x = target[:, 0]
+		y = target[:, 1]
+
+		order = np.argsort(x)
+		x.sort()
+
+		sorted_data = list()
+		for i, j in enumerate(order):
+			sorted_data.append((x[i], y[j]))
+
+		sorted_data = array(sorted_data)
+
+		del_target = list()
+		for i in range(len(sorted_data) - 1):
+			dis = np.abs(sorted_data[i][0] - sorted_data[i][1]) ** 2 + np.abs(
+				sorted_data[i + 1][0] - sorted_data[i + 1][1]) ** 2
+			if dis <= 0.001:
+				del_target.append(i)
+
+		for i in reversed(del_target):
+			sorted_data = np.delete(sorted_data, i, axis=0)
+
+		self.centers = sorted_data
+		self.cluster_number = len(sorted_data)
+
+	def rm_noise(self):
+		del_target = list()
+		processed_data = self.centers
+
+		def square_fitting(x1, y1):
+			def fun(x, a, b, c):
+				return a * x + b * x ** 2 + c
+
+			coefficient, _ = curve_fit(fun, x1, y1)
+			return coefficient
+
+		coefficient1 = square_fitting(processed_data[:, 0], processed_data[:, 1])
+
+		def r_fun(x):
+			return coefficient1[0] * x + coefficient1[1] * x ** 2 + coefficient1[2]
+
+		# for i in range(len(processed_data)):
+		# 	print()
+
+		for i in range(len(processed_data)):
+			if np.abs(r_fun(processed_data[i, 0]) - processed_data[i, 1]) > 0.5:
+				del_target.append(i)
+
+		for i in reversed(del_target):
+			processed_data = np.delete(processed_data, i, axis=0)
+
+		self.centers = processed_data
+		self.cluster_number = len(processed_data)
+		return processed_data
 
 
 class ProwessDraw:
@@ -330,14 +390,19 @@ class ProwessDraw:
 
 	"""
 
-	def __init__(self, raw_data, clustered_data, raw_point=True, save=False, true_pair=False, result_point=False,
-	             centers=False, show=True):
+	def __init__(self, raw_data, clustered_data, fitting_function=None, raw_point=True, save=False, true_pair=False,
+	             result_point=False,
+	             centers=False, show=True, fitting=False, brief=True, self_centers=None):
 		self.show = show
 		self.true_pair = true_pair
 		self.raw_point = raw_point
 		self.save = save
 		self.result_point = result_point
 		self.centers = centers
+		self.fitting = fitting
+		self.brief = brief
+		self.fitting_function = fitting_function
+		self.self_centers = self_centers
 
 		self.LOW = min(raw_data.pwr_axis_x[raw_data.wanted_data_ind])
 		self.UP = max(raw_data.pwr_axis_x[raw_data.wanted_data_ind])
@@ -345,24 +410,47 @@ class ProwessDraw:
 
 		self.go_draw(raw_data, clustered_data)
 
+		if clustered_data.method_name == 'GMM_EM' or clustered_data.method_name == 'GMM_Dirichlet':
+			self.raw_point = False
+
 	def go_draw(self, raw_data, clustered_data):
+		plt.figure(figsize=(5, 6))
 		name = ''
 		name += clustered_data.method_name
 
 		if self.raw_point:
 			plt.imshow(raw_data.wanted_data, cmap='seismic')
-			name += '_Raw'
+			if not self.brief:
+				name += '_Raw'
 
-		name += '_Normalized=' + str(raw_data.is_normalized)
-		name += 'Threshold=' + str(raw_data.threshold)
+		if not self.brief:
+			name += '_Normalized=' + str(raw_data.is_normalized)
+			name += '_Threshold=' + str(raw_data.threshold)
 
 		ax = plt.gca()
 		ax.xaxis.set_ticks_position('top')
 		plt.yticks(np.linspace(0, 349, 5), np.linspace(0, 6980, 5))
 		plt.xticks(np.linspace(0, self.LONG - 1, 5), np.linspace(self.LOW, self.UP, 5))
 
+		if self.fitting:
+			if self.fitting_function is None:
+				return '没有传入拟合出来的函数哦!'
+			x_new = np.linspace(0, self.LONG - 1, 30)
+			y_new = self.fitting_function.result_function(x_new)
+			del_target = list()
+			for i in range(len(y_new)):
+				if y_new[i] > 349 or y_new[i] < 0:
+					del_target.append(i)
+
+			for i in reversed(del_target):
+				x_new = np.delete(x_new, i, axis=0)
+				y_new = np.delete(y_new, i, axis=0)
+
+			plt.plot(x_new, y_new, '--')
+			name += '_FittingMethod=' + str(self.fitting_function.method)
+
 		if self.true_pair:
-			plt.plot(raw_data.true_pair[:, 1] - 1, raw_data.true_pair[:, 0], '*', color='red')
+			plt.plot(raw_data.true_pair[:, 1] - 1, raw_data.true_pair[:, 0], '*', color='red', alpha=0.25)
 			name += '_TruePair'
 
 		if self.result_point:
@@ -371,13 +459,87 @@ class ProwessDraw:
 			name += '_ResultPoint_ClusterNumber=' + str(clustered_data.cluster_number)
 
 		if self.centers:
-			x, y = raw_data.reverse_clean(clustered_data.centers)
-			plt.scatter(x, y, s=8, color='green')
+			if self.self_centers is None:
+				x, y = raw_data.reverse_clean(clustered_data.centers)
+			else:
+				x, y = self.self_centers[:, 0], self.self_centers[:, 1]
+			plt.scatter(x, y, s=10, color='yellow')
 			name += '_Centers'
 
+		all_way = ['K-means', 'DBSCAN', 'GMM_EM', 'GMM_Dirichlet']
+		flag = all_way.index(clustered_data.method_name)
+		if flag == 0:
+			name += '_n=' + str(clustered_data.cluster_number)
+		elif flag == 1:
+			name += '_Eps=' + str(clustered_data.eps) + '_MSA=' + str(clustered_data.msa)
+		elif flag == 2:
+			name += '_n=' + str(clustered_data.n) + '_CovType=' + str(clustered_data.cov_type)
+		elif flag == 3:
+			name += '_Real_n=' + str(clustered_data.n) + '_n=' + str(clustered_data.cluster_number) + '_CovType=' + str(
+				clustered_data.cov_type) + '_PriorType=' + str(
+				clustered_data.prior_type) + '_Prior=' + str(np.round(clustered_data.prior, 2))
+
 		if self.save:
-			name += name + '.pdf'
+			name += '.pdf'
 			plt.savefig(name, transparent=True, dpi=600, pad_inches=0, bbox_inches='tight')
 
 		if self.show:
 			plt.show()
+
+
+class ChumFitting:
+	"""
+
+
+	"""
+
+	def __init__(self, raw_data):
+		self.data = raw_data
+		self.result_function = list()
+		self.x, self.y = self.data[:][0], self.data[:][1]
+		print('现在对聚类结果进行曲线拟合, 请输入 1, 2 或者 3. \n'
+		      '1 表示用二次曲线进行拟合; \n'
+		      '2 表示用普通的插值法进行拟合; \n'
+		      '3 表示用样条插值法进行拟合. ')
+		m = int(input())
+		if m == 1:
+			self.method = 'Square_Curve'
+			print('您选择了二次曲线, 接下来开始拟合!')
+			print('请输入 1 或者 2, 某一个数字报错就换另一种!')
+			n = int(input('n='))
+			if n == 1:
+				coefficient = self.square_fitting1()
+
+				def r_fun(x):
+					return coefficient[0] * x + coefficient[1] * x ** 2 + coefficient[2]
+
+				self.result_function = r_fun
+			elif n == 2:
+				coefficient = self.square_fitting2()
+
+				def r_fun(x):
+					return coefficient[0] * x + coefficient[1] * x ** 2
+
+				self.result_function = r_fun
+		elif m == 2:
+			self.method = 'Interpolation'
+			print('您选择了插值方法, 接下来开始拟合!')
+			self.result_function = interp1d(self.x, self.y, fill_value="extrapolate")
+		elif m == 3:
+			self.method = 'Interpolation'
+			print('您选择了样条插值方法, 接下来开始拟合!')
+			self.result_function = UnivariateSpline(self.x, self.y)
+
+	def square_fitting1(self):
+		def fun1(x, a, b, c):
+			return a * x + b * x ** 2 + c
+
+		coefficient, _ = curve_fit(fun1, self.x, self.y)
+		return coefficient
+
+	def square_fitting2(self):
+		def fun2(x, a, b):
+			return a * x + b * x ** 2
+
+		coefficient, _ = curve_fit(fun2, self.x, self.y)
+		return coefficient
