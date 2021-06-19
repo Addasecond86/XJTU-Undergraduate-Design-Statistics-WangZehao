43610e746d1d5f78eaa38a8d4f79af7aa5a6275d
Wang Zehao <ZehaoWang86@outlook.com>
2021-06-19 17:30:42 +0800

    Update annotation

--- codes/PickVel.py
+++ codes/PickVel.py
@@ -1,46 +1,45 @@
 import numpy as np
 from matplotlib import pyplot as plt
 from numpy import array
-from scipy.interpolate import interp1d, UnivariateSpline
+from scipy.interpolate import UnivariateSpline, interp1d
 from scipy.optimize import curve_fit
 from sklearn import preprocessing
-from sklearn.cluster import DBSCAN
-from sklearn.cluster import KMeans
-from sklearn.mixture import BayesianGaussianMixture
-from sklearn.mixture import GaussianMixture
+from sklearn.cluster import DBSCAN, KMeans
+from sklearn.mixture import BayesianGaussianMixture, GaussianMixture
 from tqdm import trange
 
 
 class ForesightDataProcess:
 	"""
-	完成数据处理工作
+	This class is used to do the processing of the raw data.
+
 	__init__:
-		需指定道集cdp_e和道号line_e.
-		初始化过程会自动从元数据集中提取出该道集与道号对应的数据和人工拾取的速度点.
+			Read the data, and also read the manual analysis results.
+			You need to specify the cdp number 'cdp_e' and the gather number 'line_e'.
 
 	open_data:
-		根据路径读取原数据. 储存在self.sth, self.sh, self.data里.
+			Function for reading data.
 
 	get_wanted_data:
-		在原数据中根据道集和道号进行筛选. 储存在wanted_data中.
+			Function to filter the raw data. Get the data to be processed, and store it in 'wanted_data'.
 
 	get_true_data:
-		根据道集和道号筛选出人工拾取的速度点.
+			Function for reading manual analysis results.
 
 	get_pre_cluster_data:
-		进一步处理, 得到待聚类的数据.
+			Normalize functions.
 
 	clean:
-		按照threshold和是否正则化对数据进行清洗.
+			Data Cleaning.
 
 	reverse_clean:
-		对正则化后的数据进行还原.
+			Function to restore the cleaned data.
 	"""
 
-	def __init__(self, cdp_e: int = 0, line_e: int = 0):
-		sth_path = "data/速度谱/STH_combin_pwr.txt"
-		sh_path = "data/速度谱/SH_combin_pwr.txt"
-		data_path = "data/速度谱/data_combin_pwr.npy"
+	def __init__(self, cdp_e: int = 0, line_e: int = 0,
+	             sth_path="data/速度谱/STH_combin_pwr.txt",
+	             sh_path="data/速度谱/SH_combin_pwr.txt",
+	             data_path="data/速度谱/data_combin_pwr.npy"):
 
 		self.sth = list()
 		self.sh = list()
@@ -58,7 +57,7 @@ class ForesightDataProcess:
 		self.true_pair = list()
 
 		if cdp_e == 0 or line_e == 0:
-			print("cdp道集和道号未指定, 请指定后重新实例化!")
+			print("Cdp number or Gather number is not specified, please specify and re-instantiate! ")
 		else:
 			self.get_wanted_data(cdp_e, line_e)
 			self.get_true_data()
@@ -66,7 +65,8 @@ class ForesightDataProcess:
 			self.pre_cluster_data = list()
 			self.get_pre_cluster_data()
 
-			print('接下来需要指定threshold与是否normalize, 通过clean方法对数据进行清洗!')
+			print(
+				'Next, you need to specify the threshold and whether to normalize, and clean the data by the clean method! ')
 
 			self.threshold = 0
 			self.is_normalized = True
@@ -87,16 +87,17 @@ class ForesightDataProcess:
 
 	def get_wanted_data(self, cdp_e, line_e):
 		"""
-		:param cdp_e: 期望处理的cdp道集.
-		:param line_e: 期望处理的line道号.
-		return: 处理好的数据.
+		return: Processed data.
 		"""
 		self.cdp_E = cdp_e
 		self.line_E = line_e
+
 		cdp_points = self.sth['cdp']
 		line_number = self.sth['Inline3D']
+
 		self.pwr_axis_y = np.array(self.sh['time'] * 1000)
 		self.pwr_axis_x = np.array(self.sth['offset'])
+
 		wanted_data_ind = list()
 
 		for i in trange(len(cdp_points)):
@@ -109,7 +110,7 @@ class ForesightDataProcess:
 
 	def get_true_data(self):
 		"""
-		return: 与self.cdp_e和self.line_e对应的人工拾取的速度点.
+		return: Manual analysis of speed points.
 		"""
 		path = "data/AI_VelVirtual_Line_Set_PP_Velo_AI.gss"
 		l_ind = 0
@@ -119,7 +120,7 @@ class ForesightDataProcess:
 		line_number = 'LINE' + str(self.line_E)
 		cmp_number = 'CMP' + str(self.cdp_E)
 
-		# 在整个拾取数据文件里做搜索
+		# Searching through the entire pickup data file.
 		with open(path, 'r') as f:
 			lines = f.readlines()
 
@@ -148,15 +149,16 @@ class ForesightDataProcess:
 
 		true_pair = np.array(true_pair)
 		true_pair[:, 0] = true_pair[:, 0] / 20
-		true_pair[:, 1] = (true_pair[:, 1] - min(self.pwr_axis_x[self.wanted_data_ind])) / 20
+		true_pair[:, 1] = (true_pair[:, 1] -
+		                   min(self.pwr_axis_x[self.wanted_data_ind])) / 20
 
 		self.true_pair = true_pair
-		print("已生成人工拾取的速度点, 储存在self.true_pair中!")
+		print("The velocity points of the manual analysis have been generated, and stored in self.true_pair! ")
 		return true_pair
 
 	def get_pre_cluster_data(self):
 		"""
-		return: 可以直接进行聚类的数据.
+		return: The data can be directly clustered.
 		"""
 		temp = self.wanted_data.T.reshape(np.size(self.wanted_data))
 		x = len(self.wanted_data[0])
@@ -172,10 +174,11 @@ class ForesightDataProcess:
 
 	def clean(self, threshold: float = 0.01, normalized: bool = True):
 		"""
-		return: 在待聚类数据上清洗后的数据.
+		return: Cleaned data.
 		"""
 		self.threshold = threshold
 		self.is_normalized = normalized
+
 		my_data = self.pre_cluster_data[:, 2]
 		my_data = my_data.reshape(-1, 1)
 		min_val = my_data.min()
@@ -186,8 +189,10 @@ class ForesightDataProcess:
 		ind = np.array(ind, dtype=int)[0]
 
 		if normalized:
-			scaler = preprocessing.StandardScaler().fit(self.pre_cluster_data[ind, :2])
-			normalized_data = scaler.fit_transform(self.pre_cluster_data[ind, :2])
+			scaler = preprocessing.StandardScaler().fit(
+				self.pre_cluster_data[ind, :2])
+			normalized_data = scaler.fit_transform(
+				self.pre_cluster_data[ind, :2])
 		else:
 			scaler = False
 			normalized_data = self.pre_cluster_data[ind, :2]
@@ -210,8 +215,11 @@ class ForesightDataProcess:
 
 class ImpeccableCluster:
 	"""
-		占个位置
-		"""
+	This class is used to implement clustering.
+
+	__init__:
+			The clustering is done directly in this function, and all other functions are auxiliary functions.
+	"""
 
 	def __init__(self, method_name, data):
 		self.data = data
@@ -219,34 +227,45 @@ class ImpeccableCluster:
 		self.labels = list()
 		self.method_name = method_name
 		self.cluster_number = 0
+
 		all_way = ['K-means', 'DBSCAN', 'GMM_EM', 'GMM_Dirichlet']
+
 		if method_name not in all_way:
-			print('请传入聚类方法, 在K-means, DBSCAN, GMM_EM, GMM_Dirichlet中选择. ')
+			print('Please pass in the clustering method, choose among "K-means", "DBSCAN", "GMM_EM", "GMM_Dirichlet". ')
 		else:
 			flag = all_way.index(method_name)
+
 			if flag == 0:
-				print('您选择了K-means方法进行聚类, 请输入聚类中心个数n开始聚类. ')
+				print(
+					'You have selected the K-means method for clustering, please enter the number of cluster centers n to start clustering. ')
 				n = int(input())
+
 				self.cluster_k_means(n)
 			elif flag == 1:
-				print('您选择了DBSCAN方法进行聚类, 请输入领域半径Eps和最小个数MSA开始聚类. ')
+				print(
+					'You have selected the DBSCAN method for clustering, please enter the domain radius Eps and the minimum number of MSA to start clustering. ')
 				eps, msa = float(input('Eps: ')), int(input('MinSample: '))
+
 				self.eps = eps
 				self.msa = msa
 				self.cluster_dbscan(eps, msa)
 			elif flag == 2:
 				print(
-					'您选择了基于EM算法的高斯混合模型进行聚类, 请输入协方差矩阵类型con_type和聚类中心个数n开始聚类. ')
+					'You have selected a Gaussian mixture model based on EM algorithm for clustering, please enter the covariance matrix type con_type and the number of clustering centers n to start clustering. ')
 				n, cov_type = int(input('n: ')), input('Cov_type: ')
+
 				self.n = n
+
 				if not cov_type:
 					cov_type = 'full'
+
 				self.cov_type = cov_type
 				self.cluster_gmm_em(n, cov_type)
 			elif flag == 3:
 				print(
-					'您选择了基于变分推断的Dirichlet过程高斯混合模型进行聚类, 请输入协方差矩阵类型con_type, 聚类中心个数n, 先验分布类型prior_type, 先验分布参数prior开始聚类. '
+					'You have selected the Dirichlet process Gaussian mixture model based on variational inference for clustering, please enter the covariance matrix type con_type, the number of clustering centers n, the prior distribution type prior_type, and the prior distribution parameter prior to start clustering. '
 				)
+
 				n, cov_type, prior_type, prior = input('n: '), input(
 					'Cov_type: '), input('Prior_type: '), input('Prior: ')
 				if not n:
@@ -261,6 +280,7 @@ class ImpeccableCluster:
 					prior = 1. / n
 				else:
 					prior = float(prior)
+
 				self.n = n
 				self.cov_type = cov_type
 				self.prior_type = prior_type
@@ -270,6 +290,7 @@ class ImpeccableCluster:
 	def cluster_k_means(self, n=10):
 		cluster = KMeans(n)
 		cluster.fit(self.data)
+
 		self.centers = cluster.cluster_centers_
 		self.labels = cluster.labels_
 		self.cluster_number = n
@@ -277,15 +298,18 @@ class ImpeccableCluster:
 	def cluster_dbscan(self, eps, msa):
 		cluster = DBSCAN(eps=eps, min_samples=msa)
 		cluster.fit(self.data)
+
 		self.labels = cluster.labels_
 
 		point_list = np.unique(cluster.labels_)
 		centers = list()
+
 		for i in point_list[1:]:
 			ind = np.where(cluster.labels_ == i)
 			x = np.mean(self.data[ind][:, 0])
 			y = np.mean(self.data[ind][:, 1])
 			centers.append([x, y])
+
 		self.centers = array(centers)
 
 		if -1 in cluster.labels_:
@@ -296,13 +320,14 @@ class ImpeccableCluster:
 	def cluster_gmm_em(self, n, cov_type):
 		cluster = GaussianMixture(n_components=n, covariance_type=cov_type)
 		cluster.fit(self.data)
+
 		if cluster.converged_:
-			print('恭喜! 算法收敛!')
+			print('Congratulations! The algorithm converges!')
 			self.centers = cluster.means_
 			self.labels = False
 			self.cluster_number = n
 		else:
-			print('不好意思哈, 算法没收敛, 请重试或更改参数后重试!')
+			print('Sorry, the algorithm did not converge, please try again or change the parameters and try again!')
 
 	def cluster_gmm_dirichlet(self, n, cov_type, prior_type, prior):
 		"""
@@ -318,16 +343,17 @@ class ImpeccableCluster:
 		                                  weight_concentration_prior_type=prior_type, weight_concentration_prior=prior,
 		                                  max_iter=300)
 		cluster.fit(self.data)
+
 		if cluster.converged_:
-			print('恭喜! 算法收敛!')
+			print('Congratulations! The algorithm converges!')
 			self.rm_repeat_point(cluster.means_)
 			self.labels = False
 		else:
-			print('不好意思哈, 算法没收敛, 请重试或更改参数后重试!')
+			print('Sorry, the algorithm did not converge, please try again or change the parameters and try again!')
 
 	def rm_repeat_point(self, target):
 		"""
-		去除重复的点
+		Removing duplicate points.
 		"""
 		x = target[:, 0]
 		y = target[:, 1]
@@ -365,13 +391,11 @@ class ImpeccableCluster:
 			coefficient, _ = curve_fit(fun, x1, y1)
 			return coefficient
 
-		coefficient1 = square_fitting(processed_data[:, 0], processed_data[:, 1])
+		coefficient1 = square_fitting(
+			processed_data[:, 0], processed_data[:, 1])
 
 		def r_fun(x):
 			return coefficient1[0] * x + coefficient1[1] * x ** 2 + coefficient1[2]
-
-		# for i in range(len(processed_data)):
-		# 	print()
 
 		for i in range(len(processed_data)):
 			if np.abs(r_fun(processed_data[i, 0]) - processed_data[i, 1]) > 0.5:
@@ -387,7 +411,7 @@ class ImpeccableCluster:
 
 class ProwessDraw:
 	"""
-
+	The class used to integrate drawing.
 	"""
 
 	def __init__(self, raw_data, clustered_data, fitting_function=None, raw_point=True, save=False, true_pair=False,
@@ -430,11 +454,12 @@ class ProwessDraw:
 		ax = plt.gca()
 		ax.xaxis.set_ticks_position('top')
 		plt.yticks(np.linspace(0, 349, 5), np.linspace(0, 6980, 5))
-		plt.xticks(np.linspace(0, self.LONG - 1, 5), np.linspace(self.LOW, self.UP, 5))
+		plt.xticks(np.linspace(0, self.LONG - 1, 5),
+		           np.linspace(self.LOW, self.UP, 5))
 
 		if self.fitting:
 			if self.fitting_function is None:
-				return '没有传入拟合出来的函数哦!'
+				return 'There is no function passed into the fitted out! '
 			x_new = np.linspace(0, self.LONG - 1, 30)
 			y_new = self.fitting_function.result_function(x_new)
 			del_target = list()
@@ -450,12 +475,15 @@ class ProwessDraw:
 			name += '_FittingMethod=' + str(self.fitting_function.method)
 
 		if self.true_pair:
-			plt.plot(raw_data.true_pair[:, 1] - 1, raw_data.true_pair[:, 0], '*', color='red', alpha=0.25)
+			plt.plot(
+				raw_data.true_pair[:, 1] - 1, raw_data.true_pair[:, 0], '*', color='red', alpha=0.25)
 			name += '_TruePair'
 
 		if self.result_point:
-			reversed_data_x, reversed_data_y = raw_data.reverse_clean(raw_data.normalized_data)
-			plt.scatter(reversed_data_x, reversed_data_y, c=clustered_data.labels, s=1)
+			reversed_data_x, reversed_data_y = raw_data.reverse_clean(
+				raw_data.normalized_data)
+			plt.scatter(reversed_data_x, reversed_data_y,
+			            c=clustered_data.labels, s=1)
 			name += '_ResultPoint_ClusterNumber=' + str(clustered_data.cluster_number)
 
 		if self.centers:
@@ -481,7 +509,8 @@ class ProwessDraw:
 
 		if self.save:
 			name += '.pdf'
-			plt.savefig(name, transparent=True, dpi=600, pad_inches=0, bbox_inches='tight')
+			plt.savefig(name, transparent=True, dpi=600,
+			            pad_inches=0, bbox_inches='tight')
 
 		if self.show:
 			plt.show()
@@ -489,24 +518,26 @@ class ProwessDraw:
 
 class ChumFitting:
 	"""
-
-
+	The class used to fit the clustering results.
 	"""
 
 	def __init__(self, raw_data):
 		self.data = raw_data
 		self.result_function = list()
 		self.x, self.y = self.data[:][0], self.data[:][1]
-		print('现在对聚类结果进行曲线拟合, 请输入 1, 2 或者 3. \n'
-		      '1 表示用二次曲线进行拟合; \n'
-		      '2 表示用普通的插值法进行拟合; \n'
-		      '3 表示用样条插值法进行拟合. ')
+		print('Now to curve-fit the clustering results, enter 1, 2 or 3. \n'
+		      '1 denotes fitting with a quadratic curve; \n'
+		      '2 denotes fitting by the ordinary interpolation method; \n'
+		      '3 indicates fitting by spline interpolation. ')
+
 		m = int(input())
+
 		if m == 1:
 			self.method = 'Square_Curve'
-			print('您选择了二次曲线, 接下来开始拟合!')
-			print('请输入 1 或者 2, 某一个数字报错就换另一种!')
+			print('You have selected the quadratic curve, so start fitting it!')
+			print('Please enter 1 or 2, if one of the numbers is wrong, change the other!')
 			n = int(input('n='))
+
 			if n == 1:
 				coefficient = self.square_fitting1()
 
@@ -521,13 +552,17 @@ class ChumFitting:
 					return coefficient[0] * x + coefficient[1] * x ** 2
 
 				self.result_function = r_fun
+
 		elif m == 2:
 			self.method = 'Interpolation'
-			print('您选择了插值方法, 接下来开始拟合!')
-			self.result_function = interp1d(self.x, self.y, fill_value="extrapolate")
+			print('You have chosen the interpolation method, so start fitting!')
+
+			self.result_function = interp1d(
+				self.x, self.y, fill_value="extrapolate")
+
 		elif m == 3:
 			self.method = 'Interpolation'
-			print('您选择了样条插值方法, 接下来开始拟合!')
+			print("You have chosen the spline interpolation method, so let's start the fitting!")
 			self.result_function = UnivariateSpline(self.x, self.y)
 
 	def square_fitting1(self):
